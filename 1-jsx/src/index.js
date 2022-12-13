684f1d8ee3d6a3b6301eba9d4c0c4827ff11b177
David G Chaves <davidgchaves@gmail.com>
2022-12-13 16:39:09 +0100

    Add first Component

--- 1-jsx/src/index.js
+++ 1-jsx/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1) Import
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) Reference to div ID root
+const div = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) Create component
+const App = () => <h1>Hi there!</h1>;
+
+// 4) React takes control and shows the component on the screen
+ReactDOM.createRoot(div).render(<App />);
