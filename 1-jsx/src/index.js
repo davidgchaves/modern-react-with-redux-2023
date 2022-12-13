b6645c3c126f919b0a90be61d5446331f825860f
David G Chaves <davidgchaves@gmail.com>
2022-12-13 16:45:58 +0100

    Add random hello/bye there

--- 1-jsx/src/index.js
+++ 1-jsx/src/index.js
@@ -6,7 +6,15 @@ import ReactDOM from "react-dom/client";
 const div = document.getElementById("root");
 
 // 3) Create component
-const App = () => <h1>Hi there!</h1>;
+const App = () => {
+  const message = Math.random() > 0.5 ? "Hello there!" : "Bye there!";
+
+  return (
+    <h1>
+      {message} at {new Date().toLocaleTimeString()}
+    </h1>
+  );
+};
 
 // 4) React takes control and shows the component on the screen
 ReactDOM.createRoot(div).render(<App />);
