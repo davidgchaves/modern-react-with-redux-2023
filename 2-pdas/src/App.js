cbc55b3cc0e06c40918c86be4fead837183dd546
David G Chaves <davidgchaves@gmail.com>
2022-12-15 18:17:44 +0100

    2-pdas: finish styling

--- 2-pdas/src/App.js
+++ 2-pdas/src/App.js
@@ -7,23 +7,38 @@ import SiriImage from "./images/siri.png";
 const App = () => {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
 
       <div className="container">
         <section className="section">
           <div className="columns">
             <div className="column is-3">
-              <ProfileCard title="Alexa" handle="@alexa" image={AlexaImage} />
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa"
+                image={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things"
+              />
             </div>
             <div className="column is-3">
               <ProfileCard
                 title="Cortana"
                 handle="@cortana"
                 image={CortanaImage}
+                description="Cortana was made by Microsoft. Who knows what it does?"
               />
             </div>
             <div className="column is-3">
-              <ProfileCard title="Siri" handle="@siri" image={SiriImage} />
+              <ProfileCard
+                title="Siri"
+                handle="@siri"
+                image={SiriImage}
+                description="Siri was made by Apple and is being phased out"
+              />
             </div>
           </div>
         </section>
