6844a753c811a31215a68167d8eb3749c650f99b
David G Chaves <davidgchaves@gmail.com>
2022-12-19 16:21:29 +0100

    3-animals: add state hook

--- 3-animals/src/App.js
+++ 3-animals/src/App.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
   const onButtonClick = () => {
-    console.log("Animal button was clicked!");
+    setCount(count + 1);
   };
 
   return (
     <div>
       <button onClick={onButtonClick}>Add Animal</button>
+      <div>Number of animals: {count}</div>
     </div>
   );
 };
