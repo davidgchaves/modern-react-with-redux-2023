5a852b463d05b577c34f6b959d55a602a120db42
David G Chaves <davidgchaves@gmail.com>
2022-12-19 17:44:36 +0100

    3-animals: wire up the AnimalShow component

--- 3-animals/src/App.js
+++ 3-animals/src/App.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+const getRandomAnimal = () => {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+};
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
   const onButtonClick = () => {
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
   return (
     <div>
       <button onClick={onButtonClick}>Add Animal</button>
-      <div>Number of animals: {count}</div>
+      <div>
+        {animals.map((animal, index) => (
+          <AnimalShow type={animal} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
