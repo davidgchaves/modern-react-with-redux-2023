265f3675e19b2d62bf881d9cc1b0f832f44a0b9a
David G Chaves <davidgchaves@gmail.com>
2022-12-23 14:51:57 +0100

    4-pics: finish search function with real input text

--- 4-pics/src/components/SearchBar.js
+++ 4-pics/src/components/SearchBar.js
@@ -1,5 +1,25 @@
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+import { useState } from "react";
+
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit(term);
+  };
+
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" value={term} onChange={onInputChange} />
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
