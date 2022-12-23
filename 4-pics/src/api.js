ebab54725e94f1ea7e16d6fadb90ce34b704631c
David G Chaves <davidgchaves@gmail.com>
2022-12-23 12:16:08 +0100

    4-pics: finish search function

--- 4-pics/src/api.js
+++ 4-pics/src/api.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID tRbT9IsGQpQ9cC5Afd2nAX4M-sQqYwxzcVbww_xRldc",
     },
     params: {
-      query: "oceans",
+      query: term,
     },
   });
 
-  console.log(response);
-
-  return response;
+  return response.data.results;
 };
 
 export default searchImages;
--- 4-pics/src/index.js
+++ 4-pics/src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import searchImages from "./api";
 import App from "./App";
-
-searchImages();
 
 const rootDiv = document.getElementById("root");
 ReactDOM.createRoot(rootDiv).render(<App />);
