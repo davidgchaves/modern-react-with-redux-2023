3c88d981a910ae655756ca92fd3b75189d629019
David G Chaves <davidgchaves@gmail.com>
2022-12-26 10:32:25 +0100

    4-pics-with-fetch: refactor to use fetch instead of axios

--- 4-pics-with-fetch/src/api.js
+++ 4-pics-with-fetch/src/api.js
@@ -1,14 +1,17 @@
 const searchImages = async (term) => {
-  const response = await axios.get("https://api.unsplash.com/search/photos", {
+  const urlWithQueryParams =
+    "https://api.unsplash.com/search/photos?" +
+    new URLSearchParams({ query: term });
+
+  const response = await fetch(urlWithQueryParams, {
     headers: {
       Authorization: "Client-ID tRbT9IsGQpQ9cC5Afd2nAX4M-sQqYwxzcVbww_xRldc",
     },
-    params: {
-      query: term,
-    },
   });
 
-  return response.data.results;
+  const jsonDecoded = await response.json();
+
+  return jsonDecoded.results;
 };
 
 export default searchImages;
