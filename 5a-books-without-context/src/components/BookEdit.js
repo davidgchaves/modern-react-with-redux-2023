b3080b6d7fac6ee94500aced14175f89628d0919
David G Chaves <davidgchaves@gmail.com>
2022-12-28 12:22:24 +0100

    5a-books: wire up conditional book edit

--- 5a-books-without-context/src/components/BookEdit.js
+++ 5a-books-without-context/src/components/BookEdit.js
@@ -1,5 +1,29 @@
-const BookEdit = () => {
-  return <div>BookEdit</div>;
+import { useState } from "react";
+
+const BookEdit = ({ book }) => {
+  const [title, setTitle] = useState(book.title);
+
+  const onInputChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(`New title is ${title}`);
+  };
+
+  return (
+    <form className="book-edit" onSubmit={onFormSubmit}>
+      <label>Title</label>
+      <input
+        className="input"
+        type="text"
+        value={title}
+        onChange={onInputChange}
+      />
+      <button className="button is-prmary">Save</button>
+    </form>
+  );
 };
 
 export default BookEdit;
--- 5a-books-without-context/src/components/BookShow.js
+++ 5a-books-without-context/src/components/BookShow.js
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
+import BookEdit from "./BookEdit";
+
 const BookShow = ({ book, onDelete }) => {
-  const handleClick = () => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const onEditButtonClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const onDeleteButtonClick = () => {
     onDelete(book.id);
   };
 
   return (
     <div className="book-show">
-      {book.title}
+      {showEdit ? <BookEdit book={book} /> : <h3>{book.title}</h3>}
       <div className="actions">
-        <button className="delete" onClick={handleClick}>
+        <button className="edit" onClick={onEditButtonClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={onDeleteButtonClick}>
           Delete
         </button>
       </div>
